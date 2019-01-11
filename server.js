b9086db453a0b4cc2570fd085ab29e61e76c750c
w.benarto <w.benarto@gmail.com>
2019-01-10 21:59:39 -0800

    got response.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // import { parse } from 'recipe-ingredient-parser';
 const express = require('express');
+const axios = require('axios');
 
 const app = express();
 
@@ -21,15 +22,35 @@ const db = mysql.createConnection({
     database: "burgers_db"
 });
 
+var space = "\n";
+var header = "====== Extraordinary Liri has found this based on your query ...======";
+var footer = "================= Thank you for using Liri by W.B ====================";
 
 const queryURL = "https://api.edamam.com/search?q=salmon&app_id=e3c21f1d&app_key=3e40f04f482e04daac9d6917ba78643f&from=0&to=3&calories=591-722";
 const displayRecipes = () => {
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then((response) => {
-        console.log(response.data);
-    });
+    axios
+        .get(queryURL)
+        .then((response) => {
+            console.log(response.data.hits[0].recipe);
+            const data = response.data.hits[0].recipe;
+
+            output = space + header + space + space
+            space + "Name           :" + data.label +
+                space + "source         :" + data.uri +
+                space + "Ingredients    :" + data.ingredients;
+
+            console.log(output);
+            
+            console.log(response.data.hits[0].recipe.label);
+            console.log(response.data.hits[0].recipe.ingredientLines);
+            console.log(response.data.hits[0].recipe.totalTime);
+            console.log(response.data.hits[0].recipe.uri);//instructions
+            console.log(response.data.hits[0].recipe.image);
+            console.log(response.data.hits[0].recipe.healthLabels);
+            console.log(response.data.hits[0].recipe.dietLabels);
+            console.log(response.data.hits[0].recipe.calories);
+
+        });
 };
 
 displayRecipes();
